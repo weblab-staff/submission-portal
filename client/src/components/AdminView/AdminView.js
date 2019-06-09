d1d8ab2b8c08ef43d3acfc087cdd7ab746d18975
Matthew Farejowicz <mfarejow@mit.edu>
2019-06-09 15:12:58 -0700

    add missing AdminView comp

---
+++ client/src/components/AdminView/AdminView.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AdminView extends React.Component {
+  render() {
+    return (
+      <div>
+        AdminView!
+      </div>
+    )
+    ;
+  }
+}
+
+export default AdminView;
