255176bae62caf02f806bf6e530c046c7150aa4e
Cory Lynch <cjl2625@gmail.com>
2019-06-10 23:17:39 -0400

    Add mongoose.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
+
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const mongoose = require('mongoose');
 
 const publicPath = path.resolve(__dirname, "..", "client", "dist");
 
+mongoose.connect(process.env.MONGO_SRV, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"),
+        (err) => console.log("Error connecting to MongoDB: " + err));
 
 app.use(express.static(publicPath));
 
