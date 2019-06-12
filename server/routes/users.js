4a2a983f61e3e4f582d77288f5387fa2841d1567
Aaron <asipser@mit.edu>
2019-06-12 16:12:46 -0400

    updated user route to reflect API changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,21 +4,23 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  User.find({}).then(users => {
-    res.send(users);
+function find_user(id, populate, callback) {
+  const filter = id.length > 0 ? { _id: id } : {};
+  const query = User.find(filter);
+  if (populate) {
+    query.populate("team");
+  }
+  query.exec((err, data) => {
+    callback(data);
   });
+}
+
+router.get("/", (req, res) => {
+  find_user("", req.query.populate === "true", data => res.send(data));
 });
 
 router.get("/:user_id", (req, res) => {
-  User.findOne({ _id: req.params["user_id"] })
-    .then(user => {
-      res.send(user);
-    })
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500);
-    });
+  find_user(req.params["user_id"], true, data => res.send(data));
 });
 
 router.post("/:user_id/update", (req, res) => {
