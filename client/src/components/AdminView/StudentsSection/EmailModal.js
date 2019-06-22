24dc43dae35a044745f369e1fe0075b5f1e9d37d
Matthew Farejowicz <mfarejow@mit.edu>
2019-06-22 16:48:25 -0700

    small work on email fe

--- client/src/components/AdminView/StudentsSection/EmailModal.js
+++ client/src/components/AdminView/StudentsSection/EmailModal.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { post } from "../../../utils";
 
 class EmailModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: '',
+      body: '',
+    }
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });    
+  }
+
   sendEmail = () => {
-    console.log('sending email');
+    post('/api/emails/', {
+      students: this.props.selectedStudents.map(el => el._id),
+      teams: [],
+      subject: this.state.subject,
+      body: this.state.body,
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.log(err));
   }
 
   render() {
@@ -35,10 +63,10 @@ class EmailModal extends React.Component {
         </div>
         <div>
           Subject:
-          <input type='text'></input>
+          <input type='text' name='subject' onChange={this.handleChange}></input>
         </div>
         <div>
-          <textarea></textarea>
+          <textarea name='body' onChange={this.handleChange}></textarea>
         </div>
         <div>
           <button onClick={this.props.hideEmailModal}>cancel</button>
