b2f9e82cd5d8bcb8610ca4ada97dffd5be4b364b
Alex L Chen <alexchen@fb.com>
2019-07-28 18:10:20 -0700

    moved all student logic to StudentsSections

--- client/src/components/AdminView/StudentsSection/StudentsBody.js
+++ client/src/components/AdminView/StudentsSection/StudentsBody.js
@@ -9,10 +9,21 @@ class StudentsBody extends React.Component {
       selectedStudents,
       selectStudent,
       selectAll,
+      students,
+      loading,
+      modalInfo,
+      activeSort,
+      sortOrder,
+      modalActive,
+      getStudents,
       deselectStudent,
       deselectAll,
+      showInfoModal,
+      hideInfoModal,
+      handleSort,
+      isSelected,
     } = this.props;
-
+    console.log(students)
     return (
       <div>
         {activeList === "INDIVIDUAL" ? (
@@ -20,8 +31,19 @@ class StudentsBody extends React.Component {
             selectedStudents={selectedStudents}
             selectStudent={selectStudent}
             selectAll={selectAll}
+            students={students}
+            loading={loading}
+            activeSort={activeSort}
+            sortOrder={sortOrder}
+            modalActive={modalActive}
+            getStudents={getStudents}
+            getStudents={getStudents}
             deselectStudent={deselectStudent}
             deselectAll={deselectAll}
+            showInfoModal={showInfoModal}
+            hideInfoModal={hideInfoModal}
+            handleSort={handleSort}
+            isSelected={isSelected}
           />
         ) : (
           <TeamList />
