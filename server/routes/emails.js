c064548badf0ca72c2c7a0aac6950ea504bd2be5
Cory Lynch <cjl2625@gmail.com>
2019-08-09 15:43:53 -0400

    Refactor/add proper access control to email.js

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -3,16 +3,26 @@
 const express = require("express");
 const connect = require("connect-ensure-login");
 const router = express.Router();
-const utils = require("./util.js");
+const utils = require("./util");
+const ensure = require("./ensure");
+const errorWrap = require("./errorWrap");
 
-router.post("/", connect.ensureLoggedIn(), async (req, res) => {
-  if (req.user.is_admin) {
-    const user_ids = req.body.students !== undefined ? req.body.students : [];
-    const team_ids = req.body.teams !== undefined ? req.body.teams : [];
-    res.send(await utils.send_email(user_ids, team_ids, req.body.subject, req.body.body, req.user));
-  } else {
-    res.error("user is not an admin!");
-  }
-});
+router.post(
+  "/",
+  ensure.admin,
+  errorWrap(async (req, res) => {
+    const user_ids = req.body.students || [];
+    const team_ids = req.body.teams || [];
+    res.send(
+      await utils.send_email(
+        user_ids,
+        team_ids,
+        req.body.subject,
+        req.body.body,
+        req.user
+      )
+    );
+  })
+);
 
 module.exports = router;
